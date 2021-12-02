8294961a860f9cd868c98f726904da7da7dd5e25
Huxley Marvit <huxmarv@nuevaschool.org>
2021-12-02 13:59:27 -0800

    learning rust comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::net::UdpSocket;
+use std::net::UdpSocket; // import std::net::UdpSocket
 use std::net::SocketAddr;
 use stunclient::StunClient;
 
-fn main() {
-    let local_addr : SocketAddr = "0.0.0.0:0".parse().unwrap();
-    let udp = UdpSocket::bind(local_addr).unwrap();
+fn main() { // the function that cargo run runs
+    let local_addr: SocketAddr = "0.0.0.0:0".parse().unwrap(); // set let to be of type SocketAddr
+
+    // upd communications protocol
+    let udp = UdpSocket::bind(local_addr).unwrap(); // 
 
     let stunclient = StunClient::with_google_stun_server();
     let external_address = stunclient.query_external_address(&udp).unwrap();
