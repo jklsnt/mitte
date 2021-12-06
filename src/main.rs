7d63a810813e695c13cb16deae6475e6dddbea3f
Houjun Liu <hliu@shabang.cf>
2021-12-06 14:25:09 -0800

    adding UDP socket stuff

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,28 @@ use stunclient::StunClient;
 
 fn main() {
     // Define the STUN server
-    // (this is the address of stun.xten.com:3478)
-    let socket = SocketAddrV4::new(Ipv4Addr::new(216,93,246,18), 3478);
+    // (this is the address of stun.gmx.net:3478)
+    let socket = SocketAddrV4::new(Ipv4Addr::new(212,227,67,34), 3478);
     // Define the STUN Client
     let stunclient = StunClient::new(SocketAddr::V4(socket));
 
     // Bind a UDP Port to listen on and find its external address
-    let udp = UdpSocket::bind("0.0.0.0:31022").unwrap();
+    let udp = UdpSocket::bind("0.0.0.0:31021").unwrap();
     let external_address = stunclient.query_external_address(&udp).unwrap();
 
     println!("{}", external_address);
 
-    udp.connect("117.63.39.254:62056").unwrap();
+    // udp.connect("98.42.49.47:31022").unwrap();
 
-    println!("Sending message!");
-    let message_buffer = [8;10];
-    udp.send(&message_buffer).unwrap();
+    // println!("Sending message!");
+    // let message_buffer = [8;10];
+    // udp.send(&message_buffer).unwrap();
 
-    // println!("Recieving message!");
-    // let mut message_reciever = [0;10];
-    // udp.recv_from(&mut message_reciever).unwrap();
+    println!("Recieving message!");
+    let mut message_reciever = [0;10];
+    udp.recv_from(&mut message_reciever).unwrap();
 
-    // println!("Done! {:?}", &message_reciever);
+    println!("Done! {:?}", &message_reciever);
 
     // // let (size, src) = udp.recv_from(&mut message_buffer).unwrap();
     // // let (_, _) = udp.recv_from(&mut message_buffer).unwrap();
